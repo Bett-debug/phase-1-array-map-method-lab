e6576352a2ac1333b0b49fd73e2acc052d188b71
Samuel <sam3bett@gmail.com>
2025-06-26 13:03:18 +0300

    initial commit

--- index.js
+++ index.js
@@ -13,4 +13,12 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials
+    .map(tutorial => {
+      const words = tutorial.split(' ');
+      return words
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+    });
 }
+
+
